c38f88293a06ab6ff068a20f5bbad6704cbff4be
Kinh Tran Vinh <tranvinhkinh1@gmail.com>
2024-08-09 16:39:14 +0700

    add android plugin, edit pubspec.yaml

--- test/kc_foreground_service_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:kc_foreground_service/kc_foreground_service.dart';
-
-void main() {
-  test('adds one to input values', () {
-
-  });
-}
